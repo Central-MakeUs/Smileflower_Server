569c5c6ab9b322a541b93a3ac365dbbe502b8377
root <root@ip-172-31-8-169.ap-northeast-2.compute.internal>
2021-10-02 14:01:27 +0000

    fix home rank

--- src/main/java/com/smileflower/santa/src/home/HomeDao.java
+++ src/main/java/com/smileflower/santa/src/home/HomeDao.java
@@ -22,24 +22,24 @@ public class HomeDao {
 
     public List<GetHomeMountainRes> getHomeMountain(int userIdx,int mountainIdx) {
         return this.jdbcTemplate.query("select m.mountainIdx,\n" +
-                        "                m.name as mountainName,\n" +
-                        "                       m.imageUrl       as mountainImage,\n" +
-                        "                       case when m.high<500 then 1\n" +
-                        "                                           when m.high<800  then 2\n" +
-                        "                                           when m.high<1000 then 3\n" +
-                        "                                           when m.high<1300 then 4\n" +
-                        "                                           else 5 end as difficulty,\n" +
-                        "                       f.userIdx,\n" +
-                        "                       u.name as userName,\n" +
-                        "                       u.userImageUrl   as userImage,\n" +
-                        "                       COUNT(f.userIdx) as flagCount\n" +
-                        "                from flag f\n" +
-                        "                         left join mountain m on f.mountainIdx = m.mountainIdx\n" +
-                        "                         left join user u on f.userIdx = u.userIdx\n" +
-                        "                where f.mountainIdx = ?\n" +
-                        "                group by f.userIdx\n" +
-                        "                order by flagCount desc\n" +
-                        "                limit 1",
+                        "                                m.name as mountainName,\n" +
+                        "                                       m.imageUrl       as mountainImage,\n" +
+                        "                                       case when m.high<500 then 1\n" +
+                        "                                                           when m.high<800  then 2\n" +
+                        "                                                           when m.high<1000 then 3\n" +
+                        "                                                           when m.high<1300 then 4\n" +
+                        "                                                           else 5 end as difficulty,\n" +
+                        "                                       f.userIdx,\n" +
+                        "                                       u.name as userName,\n" +
+                        "                                       u.userImageUrl   as userImage,\n" +
+                        "                                       COUNT(f.userIdx) as flagCount\n" +
+                        "                                from flag f\n" +
+                        "                                         left join mountain m on f.mountainIdx = m.mountainIdx\n" +
+                        "                                         left join user u on f.userIdx = u.userIdx\n" +
+                        "                                where f.mountainIdx = ?\n" +
+                        "                                group by f.userIdx\n" +
+                        "                                order by flagCount desc, f.createdAt desc\n" +
+                        "                                limit 1",
                 (rs, rowNum) -> new GetHomeMountainRes(
                         rs.getInt("mountainIdx"),
                         rs.getString("mountainName"),
